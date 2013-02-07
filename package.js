1e00bf99592ed145a363dd1651ec86e6369f098a
Avital Oliver <avital@thewe.net>
2013-02-07 15:50:33 -0800

    Switch to using the new Meteor-NPM integration, and some small improvements

--- package.js
+++ package.js
@@ -2,9 +2,11 @@ Package.describe({
   summary: "AwsSum (Amazon Web Services API lib) repackaged for Meteor"
 });
 
+Npm.depends({
+  "awssum": "0.12.2",
+  "underscore.string": "2.3.1"
+});
+
 Package.on_use(function (api) {
-  api.use('node-modules', 'server');
-  api.use('underscore-string', 'server');
-  
   api.add_files('server.js', 'server');
 });
--- server.js
+++ server.js
@@ -1,24 +1,37 @@
+var Future = Npm.require("fibers/future");
+
+// Add the underscore string methods to the global `_` object.
+// Altenatively, once could use the underscore-string package on
+// Atmosphere, but then you'd be forced to use Meteorite. Once Meteor
+// supports third-party packages, switch over.
+_.mixin(Npm.require("underscore.string"));
 
 AWSSum = {
   load: function(moduleName, className) {
 
-    var awssum = NodeModules.require('awssum');
-    var module = awssum.load(moduleName)
+    var awssum = Npm.require('awssum');
+    var module = awssum.load(moduleName);
 
     if (!className)
       return module;
-    
+
     // Get a reference to the original interface
     var interface = awssum.load(moduleName)[className];
 
+    // Make sure to only wrap once
+    if (interface.prototype._wrappedForMeteor)
+      return interface;
+
     _.each(interface.prototype, function(fn, name) {
 
-      // Make sure it's a method and it conforms to the
-      // awssum naming scheme
+      // Make sure it's a method and it conforms to the awssum naming
+      // scheme for functions with a callback as a last argument
       if (!_.isFunction(fn) || (name !== _.classify(name)))
         return;
 
-      // Wrap it!
+      // Wrap it! Unfortunately we can't use Future.wrap, which looks at the wrapped
+      // function's arity. This doesn't work well with optional arguments, as used by
+      // awssum: https://github.com/appsattic/node-awssum/blob/master/lib/awssum.js#L325
       interface.prototype[name] = function() {
 
         var fut = new Future();
@@ -27,19 +40,19 @@ AWSSum = {
         var args = _.toArray(arguments);
 
         // If called without a callback add Future-ized callback
+        //
+        // XXX missing here: If called with a callback, wrap that
+        // callback with `Meteor.bindEnvironment`
         if (!_.isFunction(_.last(args)))
 
           // Build up a callback backed by Future
           args.push(function(error, data) {
-
-            // If there's an error set it as an attribute
-            // and return it.
-            // TODO can's we throw an error?
+            // This notifies `fut.wait()` below to either return a
+            // value or throw an exception.
             if (error)
-              data = { error: error };
-
-            // Return the data through Future
-            fut.return(data);
+              fut.throw(error);
+            else
+              fut.return(data);
           });
 
         // Run the method with our Future-ized callback
@@ -49,6 +62,7 @@ AWSSum = {
       };
     });
 
+    interface.prototype._wrappedForMeteor = true;
     return interface;
   }
 };
